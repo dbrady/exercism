b28c9a63cfa74ac6d0ee591dec022b2bd4fa57af
David Brady <dbrady@covermymeds.com>
2017-05-22 16:39:43 -0600

    Lrn2Binary in elixir yay

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -7,38 +7,27 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
-  def rotate(text, shift) do
-    text |> String.split("") |> Enum.map(&(rotate_char(&1,shift))) |> Enum.join("")
+  def rotate(<<string :: binary>>, shift) when shift == 0 or shift == 26 do
+    string
   end
 
-  def rotate_char(char, shift) when shift==0 or shift==26, do: char
-
-  def rotate_char(char, _) when char == "", do: char
-
-  def rotate_char(char, shift) do
-    # WOOF. There's GOT to be a sleek and sexy way to do this
-    # For now, do it fat and slobbery
-    plain_int = char_to_int(char)
-    base = cond do
-      plain_int in ?A..?Z -> ?A
-      plain_int in ?a..?z -> ?a
-      true -> 0
-    end
-
-    cipher_int = if base > 0 do
-      rem((plain_int - base) + shift, 26) + base
-    else
-      plain_int
-    end
-
-    int_to_char(cipher_int)
+  def rotate(<<char :: 8, tail :: binary>>, shift) do
+    <<rotate_char(char, shift)>> <> rotate(tail, shift)
   end
 
-  def char_to_int(char) do
-    elem(List.to_tuple(String.to_charlist(char)), 0)
+  def rotate(<<tail :: binary>>, shift) do
+    tail
   end
 
-  def int_to_char(int) do
-    List.to_string Tuple.to_list({int})
+  def rotate_char(char, shift) when char in ?A..?Z do
+    rem((char - ?A) + shift, 26) + ?A
+  end
+
+  def rotate_char(char, shift) when char in ?a..?z do
+    rem((char - ?a) + shift, 26) + ?a
+  end
+
+  def rotate_char(char, _shift) do
+    char
   end
 end
